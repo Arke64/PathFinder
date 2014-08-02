414fd8554bc92d29bee16559101ce0a8f26dfcd2
Arke64 <johnbrochue@outlook.com>
2014-08-02 18:23:25 -0400

    Added file parsing.

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,52 @@
 #include <Utilities/Common.h>
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
 
+class grid {
+	word width;
+	word height;
+	word start_x;
+	word start_y;
+	word end_x;
+	word end_y;
+
+	vector<vector<word>> cells;
+
+	public:
+		void parse_file(string path);
+		
+};
+
+void grid::parse_file(string path) {
+	ifstream file(path);
+
+	file >> this->width >> this->height;
+	file >> this->start_x >> this->start_y;
+	file >> this->end_x >> this->end_y;
+
+	this->cells.resize(this->width);
+
+	for (word x = 0; x < this->width; x++)
+		this->cells[x].resize(this->height);
+
+	for (word y = 0; y < this->height; y++)
+		for (word x = 0; x < this->width; x++)
+			file >> this->cells[x][y];
+}
+
 int main() {
+	string path;
+
+	cout << "Filename: ";
+	cin >> path;
+
+	grid g;
+	g.parse_file(path);
 
 	return 0;
 }
